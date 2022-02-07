98ed1dd4c155fb40f6a526cb38d46d670341ef1d
marceloamor <kornexl9@gmail.com>
2022-02-07 18:28:42 +0100

    building out server

--- server.py
+++ server.py
@@ -1,7 +1,23 @@
 # Python program to implement server side of chat room.
-# import socket
+import socket
 # import select
 # import sys
 # from thread import *
 
 print("I am a server")
+
+s = socket.socket() 
+print('Socket created')
+
+s.bind(('localhost',9999))
+
+s.listen(3)
+print('waiting for connection...')
+
+while True:
+    c, addr = s.accept()
+    print('Connected with ', addr)
+
+    c.send('Welcome to the server')
+
+    c.close()
