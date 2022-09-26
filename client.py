c9e1ea3051ec54cac70d0ae607edb4ddde2a6003
marceloamor <kornexl9@gmail.com>
2022-09-26 16:38:28 +0100

    test commit

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ print("I am a client")
 
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#be sure to hardcode IP address to make it work between machines
+#be sure to hardcode IP address to make it work between machines   
 c.connect(('localhost',9999))
 
 print(c.recv(2048).decode())
